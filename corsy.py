0f5790fc550c2670ab92d9d7f98cbbea7ca87756
vlad <daprostovseeto@gmail.com>
2021-01-20 17:09:34 +0200

    update corsy error handling1

--- corsy.py
+++ corsy.py
@@ -8,9 +8,9 @@ from core.tests import active_tests
 from core.colors import white, green, info, bad, good, grey, end
 
 try:
-	from urllib.parse import urlparse
+    from urllib.parse import urlparse
 except ImportError:
-	from urlparse import urlparse
+    from urlparse import urlparse
 
 print('''
     %sＣＯＲＳＹ  %s{%sv0.1-beta%s}%s
@@ -24,35 +24,37 @@ args = parser.parse_args()
 target_url = args.url
 delay = args.delay
 
+
 def cors(target, delay, scheme=False):
-	url = target
-	if not target.startswith(('http://', 'https://')):
-		url = scheme + '://' + url
-	parsed = urlparse(url)
-	root = host(url) or parsed.hostname
-	netloc = parsed.netloc
-	scheme = parsed.scheme
-	url = scheme + '://' + netloc
-	active = active_tests(url, root, scheme, delay)
-	return active
+    url = target
+    if not target.startswith(('http://', 'https://')):
+        url = scheme + '://' + url
+    parsed = urlparse(url)
+    root = host(url) or parsed.hostname
+    netloc = parsed.netloc
+    scheme = parsed.scheme
+    url = scheme + '://' + netloc
+    active = active_tests(url, root, scheme, delay)
+    return active
+
 
 details = load_json('./db/details.json')
 
 if target_url:
-	if target_url.startswith(('http://', 'https://')):
-		try:
-			result = cors(target_url, delay)
-			if result:
-					print('%s Misconfiguration found!' % good)
-					print('%s Title: %s' % (info, result))
-					print('%s Description: %s' % (info, details[result.lower()]['Description']))
-					print('%s Severity: %s' % (info, details[result.lower()]['Severity']))
-					print('%s Exploitation: %s' % (info, details[result.lower()]['Exploitation']))
-				else:
-					print('%s No misconfiguration found.' % bad)
-		except ConnectionError:
-			print('%s Connection refused.' % bad)
-	else:
-		print('%s Please use https://example.com not example.com' % bad)
+    if target_url.startswith(('http://', 'https://')):
+        try:
+            result = cors(target_url, delay)
+            if result:
+                print('%s Misconfiguration found!' % good)
+                print('%s Title: %s' % (info, result))
+                print('%s Description: %s' % (info, details[result.lower()]['Description']))
+                print('%s Severity: %s' % (info, details[result.lower()]['Severity']))
+                print('%s Exploitation: %s' % (info, details[result.lower()]['Exploitation']))
+            else:
+                print('%s No misconfiguration found.' % bad)
+        except ConnectionError:
+            print('%s Connection refused.' % bad)
+    else:
+        print('%s Please use https://example.com not example.com' % bad)
 else:
-	print('\n' + parser.format_help().lower())
+    print('\n' + parser.format_help().lower())
